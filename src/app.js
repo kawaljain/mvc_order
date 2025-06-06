dd1e21d2aecb70cc2fcf8a39209e40fa37edb6ac
Kawal Jain <kawaljain03@gmail.com>
2025-06-06 10:41:10 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/api/order/create", (req, res) => {
+  res.send("done");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
